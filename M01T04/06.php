9c16f1b5ae7b7e1bfef00694042f262a4de2e5e0
Steven Lara <steven.carcamo2019adoc@gmail.com>
2020-06-09 22:40:04 -0600

    terminé el archivo 06.php

---
+++ M01T04/06.php
@@ -0,0 +1,9 @@
+<?php
+
+$randomNumbers = [mt_rand(0,9), mt_rand(0,9), mt_rand(0,9), mt_rand(0,9)];
+$sumArray = $randomNumbers[0] + $randomNumbers[1] + $randomNumbers[2] + $randomNumbers[3];
+$arrayToShow = implode($randomNumbers);
+
+print_r("<center><h1>El número que sumaremos al azar es $arrayToShow</h1><center><br><br>" . "<center><h2>$randomNumbers[0]  +  $randomNumbers[1]  +  $randomNumbers[2]  +  $randomNumbers[3]  =  $sumArray</h2></center>");
+
+?>
