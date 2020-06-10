2c02b3aea77ac087037544f0d4293fb1e13c2bca
Steven Lara <steven.carcamo2019adoc@gmail.com>
2020-06-09 21:36:04 -0600

    terminé el archivo 03.php

---
+++ M01T04/03.php
@@ -0,0 +1,9 @@
+<?php
+
+$alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+foreach ($alphabet as $completed) {
+    echo strtolower($completed) ." $completed". "<br/>";
+}
+
+?>
