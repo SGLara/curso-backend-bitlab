18b641e5087163592d8e8361f0687f1144e9d22c
Steven Lara <steven.carcamo2019adoc@gmail.com>
2020-06-22 23:03:29 -0600

    I finished M01T06, finally

--- M01T06/index.php
+++ M01T06/index.php
@@ -1,50 +1,56 @@
 <?php
 
-class Blog{
+class Blog
+{
+    public $comments = 0;
+    public function postNews(string $message/* string $author*/)
+    {
+        $news = $message;
+        $author = $this->name;
+        // $author = $author;
+        echo "<b>Nuevo Post:</b><br> $news.<br><br><i>Escrito por: $author</i>";
+    }
 
-   public $comments = 0;
+    public function postReview(string $message/*, string $author*/)
+    {
+        $post = $message;
+        $author = $this->name;
+        // $author = $author;
+        echo "<b>Nueva Reseña:</b><br>$post(<b> Reseña de:  </b><i> {$this->title})</i><br><br><i>Escrito por: $author</i>";
+    }
 
-   public function postNews( string $message/* string $author*/ ) {
-    $news = $message;
-    $author = $this->name;
-    // $author = $author;
-    echo "<b>Nuevo Post:</b><br> $news.<br><br><i>Escrito por: $author</i>";
-   }
-
-   public function postReview( string $message/*, string $author*/ ) {
-    $post = $message;
-    $author = $this->name;
-    // $author = $author;
-    echo "<b>Nueva Reseña:</b><br>$post(<b> Reseña de:  </b><i> {$this->title})</i><br><br><i>Escrito por: $author</i>";
-   }
-
-   public function postComment( string $message/* string $author*/ ) {
-    global $comments;
-    $post = $message;
-    $author= $this->name;
-    // $author = $author;
-    echo "<b>Nuevo Comentario:</b><br>$post<br><br><i>Escrito por: $author</i>";
-    $comments++;
-   }
-   //NO ME FUNCIONA PARA CONTAR LOS COMENTARIOS: NI AQUI ↑, NI ACÁ ↓
-   public function totalComments() {
-       echo "El total de comentarios de reseñas y noticias es: {$this->comments}";
-   }
+    public function postComment(string $message/* string $author*/)
+    {
+        global $comments;
+        $post = $message;
+        $author = $this->name;
+        // $author = $author;
+        echo "<b>Nuevo Comentario:</b><br>$post<br><br><i>Escrito por: $author</i>";
+        $comments++;
+    }
+    //NO ME FUNCIONA PARA CONTAR LOS COMENTARIOS: NI AQUI ↑, NI ACÁ ↓
+    public function totalComments()
+    {
+        echo "El total de comentarios de reseñas y noticias es: {$this->comments}";
+    }
 }
 
-class Movies extends Blog {
+class Movies extends Blog
+{
     public $title = 'Sin titulo! Asigne un titulo porfavor';
     public $review = '';
     public $author = '';
     public $characters = [];
 }
 
-class Series extends Movies {
+class Series extends Movies
+{
     public $season = '';
     public $chapter = '';
 }
 
-class Person extends Blog{
+class Person extends Blog
+{
     public $name = '';
     public $job = '';
     public $birthday = '';
@@ -65,15 +71,15 @@ $lucifer->postNews("Se dice que habrá una nueva temporada de la famosa serie <b
 echo "<hr>";
 
 echo "Los cumpleaños de los personajes son: <br><br>";
- for ($i = 0; $i < count($lucifer->characters); $i++) { 
-     $date = mt_rand(1, 30);
-     $month = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
-     $monthRandom = mt_rand(0,(count($month)-1));
-     $person = new Person();
-     $person->name = $lucifer->characters[$i];
-     $person->birthday = $date;
-     echo "-{$person->name} cumple el {$person->birthday} de {$month[$monthRandom]} <br>";
- }
+for ($i = 0; $i < count($lucifer->characters); $i++) {
+    $date = mt_rand(1, 30);
+    $month = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    $monthRandom = mt_rand(0, (count($month) - 1));
+    $person = new Person();
+    $person->name = $lucifer->characters[$i];
+    $person->birthday = $date;
+    echo "-{$person->name} cumple el {$person->birthday} de {$month[$monthRandom]} <br>";
+}
 
 echo "<hr>";
 $carlos = new Person();
@@ -88,4 +94,3 @@ $steven->name = "SGLaravel";
 $steven->postComment("En serio que me encantan tus publicaciones amigo! ♥.");
 echo "<hr>";
 $blog->totalComments();
-?>
