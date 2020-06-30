ee2237a527af58f7961e42bc372c8831e626da5d
Steven Lara <steven.carcamo2019adoc@gmail.com>
2020-06-29 23:56:41 -0300

    I fixed some issues

---
+++ M01T08/15_multi.php
@@ -0,0 +1,13 @@
+<?php
+function multi(int $number1, int $number2)
+{
+    echo (($number1 * $number1) * $number2)."<br>-----------<br>";
+}
+
+multi(2, 5);
+multi(5, 8);
+multi(7, 5);
+multi(2, 6);
+multi(2, 7);//This is your example
+
+?>
